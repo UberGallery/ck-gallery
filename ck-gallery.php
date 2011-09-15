3bae3789b66c97cdb39783f5677e8e35cad62685
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2011-09-15 11:29:48 -0700

    v1.0.0-beta1

--- ck-gallery.php
+++ ck-gallery.php
@@ -1,9 +1,10 @@
 <?php
 
-  $galleryDir = "gallery";           // Original images directory (No trailing slash!)
-  $thumbsDir = "$galleryDir/thumbs"; // Thumbnails directory (No trailing slash!)
-  $logFile   = "gallery-log.txt";   // Directory/Name of log file
-  $thumbSize = 100;                 // Thumbnail width/height
+  $galleryDir = "gallery";            // Original images directory (No trailing slash!)
+  $thumbsDir  = "$galleryDir/thumbs"; // Thumbnails directory (No trailing slash!)
+  $logFile    = "gallery-log.txt";    // Directory/Name of log file
+  $thumbSize  = 100;                  // Thumbnail width/height in pixels
+  $imgPerPage = 0;                    // Images per page (0 disables pagination)
 
 
   // *** DO NOT EDIT ANYTHING BELOW HERE UNLESS YOU ARE A PHP NINJA ***
@@ -12,7 +13,7 @@
   // START FUNCTIONS
 
   function createThumb($source,$dest,$thumb_size) {
-    // Modified from http://www.findmotive.com/2006/08/29/create-square-image-thumbnails-with-php/
+  // Create thumbnail, modified from function found on http://www.findmotive.com/tag/php/
     $size = getimagesize($source);
     $width = $size[0];
     $height = $size[1];
@@ -27,29 +28,37 @@
 
     $new_im = ImageCreatetruecolor($thumb_size,$thumb_size);
 
-    if (strrpos($source,'.jpg') == true) {
+    @$imgInfo = getimagesize($source);
+
+    if ($imgInfo[2] == IMAGETYPE_JPEG) {
       $im = imagecreatefromjpeg($source);
-    } elseif(strrpos($source,'.gif') == true) {
+      imagecopyresampled($new_im,$im,0,0,$x,$y,$thumb_size,$thumb_size,$width,$height);
+      imagejpeg($new_im,$dest,80); // Thumbnail quality (Value from 1 to 100)
+    } elseif ($imgInfo[2] == IMAGETYPE_GIF) {
       $im = imagecreatefromgif($source);
-    } elseif(strrpos($source,'.png') == true) {
+      imagecopyresampled($new_im,$im,0,0,$x,$y,$thumb_size,$thumb_size,$width,$height);
+      imagegif($new_im,$dest);
+    } elseif ($imgInfo[2] == IMAGETYPE_PNG) {
       $im = imagecreatefrompng($source);
-    } else {
-      echo("<!-- Not a valid format. -->");
+      imagecopyresampled($new_im,$im,0,0,$x,$y,$thumb_size,$thumb_size,$width,$height);
+      imagepng($new_im,$dest);
     }
-
-    imagecopyresampled($new_im,$im,0,0,$x,$y,$thumb_size,$thumb_size,$width,$height);
-    imagejpeg($new_im,$dest,80); // Thumbnail quality
   }
-  
-  function isImage($filename) {
-    // Verifies that a file is an image
-    $imgExt = array('jpg', 'gif', 'png'); // Define image types
 
-    foreach ($imgExt as $ext) 
-      if (strrpos($filename, ".$ext")) {
+  function isImage($fileName) {
+  // Verifies that a file is an image
+    if ($fileName !== '.' && $fileName !== '..') {
+      @$imgInfo = getimagesize($fileName);
+
+      $imgType = array(
+        IMAGETYPE_JPEG,
+        IMAGETYPE_GIF,
+        IMAGETYPE_PNG,
+      );
+
+      if (in_array($imgInfo[2],$imgType))
         return true;
-      } else {
-        return false;
+      return false;
     }
   }
 
@@ -57,81 +66,162 @@
 
 
   // Create log file if it does not exist, otherwise open log for writing
-  if (file_exists($logFile) == false) {
+  if (!file_exists($logFile)) {
     $log = fopen($logFile, "a");
-    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $logFile\n\n");
+    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $logFile\r\n\r\n");
   } else {
     $log = fopen($logFile, "a");
   }
 
   // Create image directory if it doesn't exist
-  if (file_exists($galleryDir) == false) {
+  if (!file_exists($galleryDir)) {
     mkdir($galleryDir);
-    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $galleryDir\n");
+    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $galleryDir\r\n");
   }
 
   // Create thumbnail directory if it doesn't exist
-  if (file_exists($thumbsDir) == false) {
+  if (!file_exists($thumbsDir)) {
     mkdir($thumbsDir);
-    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $thumbsDir\n");
+    fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $thumbsDir\r\n");
   }
-  
-  // Alcohol! The cause of, and solution to, all of life's problems!
-  
-  // Opening markup
-  echo("<!-- Start CK-Gallery v .83 - Created by, Chris Kankiewicz [http://web-geek.net/ck-gallery] -->\n");
-  echo("<div id=\"gallery-wrapper\">\n  <div id=\"ck-gallery\">\n");
-  
-  $dir = opendir($galleryDir);
-  while (($file = readdir($dir)) !== false) {
-    // Converts file extensions to all lowercase
-    if (strrpos($file,'.JPG',1) || strrpos($file,'.GIF',1) || strrpos($file,'.PNG',1)) {
-      $srcfile = "$galleryDir/$file";
-      $filearray = explode(".",$file);
-      $pos = count($filearray) - 1;
-      $filearray[$pos] = strtolower($filearray[$pos]);
-      $file = implode(".",$filearray);
-      $dstfile = "$galleryDir/$file";
-      rename($srcfile,$dstfile); // I hate Windows
-      fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  RENAMED: $srcfile to $dstfile\n");
-    }
-    // Create thumbnail if it doesn't already exist
-    if (isImage($file)) {
-      if (file_exists("$thumbsDir/$file") == false) {
-        createThumb("$galleryDir/$file", "$thumbsDir/$file",$thumbSize);
-        fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $thumbsDir/$file\n");
+
+  // Clean up thumbnail directory
+  if ($dirHandle = opendir($thumbsDir)) {
+    while (($file = readdir($dirHandle)) !== false) {
+      if (isImage("$thumbsDir/$file")) {
+        $size = getimagesize("$thumbsDir/$file");
+        if (!file_exists("$galleryDir/$file") || $size[0] !== $thumbSize) {
+          unlink("$thumbsDir/$file");
+          fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  REMOVED: $thumbsDir/$file\r\n");
+        }
       }
     }
-    // Create XHTML compliant markup
-    if (isImage($file)) {
-      $noExt = substr($file,0,strrpos($file,'.'));
-      $altText = str_replace("_"," ",$noExt);
-      echo "    <div class=\"gallery-box\" style=\"float: left;\"><a href=\"$galleryDir/$file\" title=\"$altText\" class=\"thickbox\" rel=\"photo-gallery\"><img src=\"$thumbsDir/$file\" alt=\"$altText\" style=\"margin: 5px;\" /></a></div>\n";
+  closedir($dirHandle);
+  }
+
+  // Alcohol! The cause of, and solution to, all of life's problems!
+
+  // Create array from gallery directory
+  if ($dirHandle = opendir($galleryDir)) {
+    while (($file = readdir($dirHandle)) !== false) {
+      if (isImage("$galleryDir/$file")) {
+        $images[] = $file;
+      }
     }
+  closedir($dirHandle);
+  }
+
+  // Page varriables
+  $totalImages = count($images);
+  if ($imgPerPage <= 0 || $imgPerPage >= $totalImages) {
+    $imgStart = 0;
+    $imgEnd = $totalImages;
+  } elseif ($imgPerPage > 0 && $imgPerPage < $totalImages) {
+    $totalPages = ceil($totalImages / $imgPerPage);
+    if ($_GET['page'] < 1) {
+      $currentPage = 1;
+    } elseif ($_GET['page'] > $totalPages) {
+      $currentPage = $totalPages;
+    } else {
+      $currentPage = $_GET['page'];
+    }
+    $imgStart = ($currentPage - 1) * $imgPerPage;
+    $currentPage * $imgPerPage > $totalImages ? $imgEnd = $totalImages : $imgEnd = $currentPage * $imgPerPage;
+  }
+
+  // Opening markup
+  echo("<!-- Start CK-Gallery v1.0.0 BETA - Created by, Chris Kankiewicz [http://web-geek.net/ck-gallery] -->\r\n");
+  echo("<div id=\"gallery-wrapper\">\r\n  <div id=\"ck-gallery\">\r\n");
+
+  for ($x = $imgStart; $x < $imgEnd; $x++) {
+    $filePath = "$galleryDir/$images[$x]";
+
+    // Convert file name and extension for processing
+    if (ctype_upper(pathinfo($filePath,PATHINFO_EXTENSION))
+    || strpos(basename($filePath),' ') !== false
+    || strpos($filePath,'.jpeg') !== false) {
+
+      $source = "$filePath";
+      $fileParts = pathinfo($filePath); // Create array of file parts
+      $ext = $fileParts['extension']; // Original extension
+      $name = basename($filePath, ".$ext"); // Original file name without extension
+      $dir = $fileParts['dirname']; // Directory path
+
+      // Change extension to all lowercase
+      if (ctype_upper($ext)) {
+        $ext = strtolower($ext);
+      }
+
+      // Convert .jpeg to .jpg
+      if ($ext == 'jpeg') {
+        $ext = 'jpg';
+      }
+
+      // Replace spaces with underscores
+      if (strpos($name,' ') !== false) {
+        $extOld = $fileParts['extension'];
+        $name = str_replace(' ','_',basename($filePath, ".$extOld"));
+      }
+
+      $destination = "$dir/$name.$ext";
+
+      // Rename file and array element
+      if (rename($source,"$dir/$name.tmp")) {
+        if (rename("$dir/$name.tmp",$destination)) {
+          $images[$x] = "$name.$ext";
+          fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  RENAMED: $source to $destination\r\n");
+        }
+      }
+    }
+
+    $filePath = "$galleryDir/$images[$x]";
+    $thumbPath = "$thumbsDir/$images[$x]";
+
+    // Create thumbnail if it doesn't already exist
+    if (!file_exists("$thumbPath")) {
+      createThumb("$filePath","$thumbPath",$thumbSize);
+      fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  CREATED: $thumbsDir/$images[$x]\r\n");
+    }
+    // Create XHTML compliant markup
+    $noExt = substr($images[$x],0,strrpos($images[$x],'.'));
+    $altText = str_replace("_"," ",$noExt);
+    echo "    <div class=\"gallery-box\" style=\"float: left;\"><a href=\"$filePath\" title=\"$altText\" class=\"thickbox\" rel=\"photo-gallery\"><img src=\"$thumbPath\" alt=\"$altText\" style=\"margin: 5px;\" /></a></div>\r\n";
+  }
+
+  // Clear float, create horizontal rule
+  echo("    <div class=\"clear\" style=\"clear: both;\"></div><div class=\"hr\"><hr /></div>\r\n");
+
+  // If pagination enabled, create page navigation
+  if ($imgPerPage > 0 && $imgPerPage < $totalImages) {
+    $pageName = basename($_SERVER["PHP_SELF"]); // Get current page file name
+    echo("    <ul id=\"ck-pagination\" style=\"margin: 0; padding: 0;\">\r\n");
+
+    // Previous arrow
+    $previousPage = $currentPage - 1;
+    echo("      <li style=\"float: left; list-style: none; margin: 0 5px 0 0 !important;\"".($currentPage > 1 ? "><a href=\"$pageName?page=$previousPage\">&lt;</a>" : " class=\"inactive\">&lt;")."</li>\r\n");
+
+    // Page links
+    for ($x = 1; $x <= $totalPages; $x++) {
+      echo("      <li style=\"float: left; list-style: none; margin: 0 5px 0 0 !important;\"".($x == $currentPage ? " class=\"current-page\">$x" : "><a href=\"$pageName?page=$x\">$x</a>")."</li>\r\n");
+    }
+
+    // Next arrow
+    $nextPage = $currentPage + 1;
+    echo("      <li style=\"float: left; list-style: none; margin: 0 5px 0 0 !important;\"".($currentPage < $totalPages ? "><a href=\"$pageName?page=$nextPage\">&gt;</a>" : " class=\"inactive\">&gt;")."</li>\r\n");
+
+    echo("    </ul>\r\n");
   }
 
   // Closing markup
-  echo("    <div class=\"clear\" style=\"clear: both;\"></div>\n");
-  echo("    <div id=\"credit\" style=\"float: right;\"><small>Powered by <a href=\"http://web-geek.net/ck-gallery\" onclick=\"window.open(this.href); return false;\" onkeypress=\"window.open(this.href); return false;\">CK-Gallery</a>");
-  if(file_exists("thickbox.js") || file_exists("thickbox/thickbox.js")) { // Checks if site is using Thickbox
-    echo(" &amp; <a href=\"http://jquery.com/demo/thickbox\" onclick=\"window.open(this.href); return false;\" onkeypress=\"window.open(this.href); return false;\">Thickbox</a></small></div>\n");
+  echo("    <div id=\"credit\" style=\"float: right;\">Powered by <a href=\"http://web-geek.net/ck-gallery\" target=\"_blank\">CK-Gallery</a>");
+  // Display Thickbox link if site is using Thickbox
+  if(file_exists("thickbox.js") || file_exists("thickbox/thickbox.js")) {
+    echo(" &amp; <a href=\"http://jquery.com/demo/thickbox\" target=\"_blank\">Thickbox</a></div>\r\n");
   } else {
-    echo("</small></div>\n");
+    echo("</div>\r\n");
   }
-  echo("    <div class=\"clear\" style=\"clear: both;\"></div>\n  </div>\n</div>\n");
+  echo("    <div class=\"clear\" style=\"clear: both;\"></div>\r\n  </div>\r\n</div>\r\n");
   echo("<!-- End CK-Gallery - Licensed under the GNU Public License version 3.0 -->");
-  
-  // Clean up thumbnail directory
-  $dir = opendir($thumbsDir);
-  while (($file = readdir($dir)) !== false) {
-    if ($file !== "." && $file !== "..") {
-      $size = getimagesize("$thumbsDir/$file");
-      if (file_exists("$galleryDir/$file") == false || $size[0] !== $thumbSize) {
-        unlink("$thumbsDir/$file");
-        fwrite($log,date("Y-m-d")." @ ".date("H:i:s")."  REMOVED: $thumbsDir/$file\n");
-      }
-    }
-  }
 
   fclose($log); // Close log
 
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 /*********************************************
-*  Title:    ck-gallery.php                  *
-*  Author:   Chris Kankiewicz                *
-*  Email:    chris@web-geek.net              *
+*  TITLE:    ck-gallery.php                  *
+*  AUTHOR:   Chris Kankiewicz                *
+*  EMAIL:    Chris@Web-Geek.net              *
 *  URL:      http://web-geek.net/ck-gallery  *
 *********************************************/
 
@@ -9,14 +9,34 @@
 Introduction
 ----------------------------------------
 
-  CK-Gallery is a simple, yet powerful PHP photo gallery that will basically 
-  manage itself for you. CK-Gallery supports .jpg, .gif & .png image types, and 
-  will automatically creates thumbnails on the fly. The main feature of this 
-  gallery is it automatically outputs XHTML compliant markup for inclusion on a 
-  web page. The CK-Gallery will also automatically prune it's thumbnails, so when 
-  you delete an image from the images folder, it will delete the corresponding 
-  thumbnail. The gallery also creates a log file for reference and debugging.
-  The CK-Gallery is licensed under the GNU General Public License version 3.0.
+  CK-Gallery is a simple, yet powerful, PHP photo gallery that will basically
+  manage itself for you. CK-Gallery supports .jpg, .gif & .png image types, and
+  will automatically creates thumbnails on the fly. The main feature of this
+  gallery is it automatically outputs XHTML compliant markup for inclusion on a
+  web page. The CK-Gallery will also automatically prune it's thumbnails, so
+  when you delete an image from the images folder, it will delete the
+  corresponding thumbnail. The gallery also creates a log file for reference and
+  debugging.  The CK-Gallery is licensed under the GNU General Public License
+  version 3.0.
+
+  With the update to version 1.0.0 CK-Gallery now has dynamic pagination.  This
+  will let users with larger galleries split the gallery up into several smaller
+  pages and allow visitors to easily navigate to those pages via a small
+  navigation bar below the gallery.
+
+
+Requirements
+----------------------------------------
+
+  Due to some of the functions used, you must have PHP version 4.0.6 or higher
+  on Linux, or verstion 4.3.0 on Windows. Also, you must have the PHP GD version
+  2.0.1 or later installed (2.0.28 or later is recommended).
+
+  The lowest PHP version this script has been successfully tested on was a Linux
+  system running PHP version 4.3.11 and PHP GD version 2.0.28, if you've got it
+  to run on a lower version I'd like to know, please contact me.
+
+  For more information on PHP GD, please see http://us.php.net/gd
 
 
 Included Files
@@ -36,38 +56,53 @@ Included Files
 Simple Installation
 ----------------------------------------
 
-  1. Upload the entire contents of this directory to your web server in the 
+  1. Upload the entire contents of this directory to your web server in the
      directory where you would like the gallery to be displayed.
      Example: http://www.domain-name.com/photo-gallery/
   2. Upload your images to the /gallery directory.
-  3. Navigate to the directory where you installed the gallery and the script 
-     should generate thumbnails and display your pictures.
+  3. Open your web browser and navigate to the directory where you installed the
+     gallery and the script should generate thumbnails and display your pictures
+
+
+Pagination
+----------------------------------------
+
+  To enable pagination, open ck-gallery.php, find "$imgPerPage = 0;" at the top
+  and set this value to the number of images you would like to display per page.
+
+  NOTE: If pagination is enabled, the script will generate thumbnails on a
+  per-page basis.  You do not have to worry about this though, the first time a
+  visitor visits a page without thumbnails, it will automatically generate the
+  thumbnails, however this may cause the page to take several seconds to load
+  while this processes.
 
 
 Install to Pre-Existing Web Page
 ----------------------------------------
 
-  1. If you wish to change the name or location of the images, thumbs, or log 
-     file, open ck-gallery.php and edit the $gallerydir, $thumbsdir, and 
-     $logfile variables found at the top of the script.
+  1. If you wish to change the name or location of the images folder, thumbnails
+     folder, or log file, or you would like to enable pagination, open
+     ck-gallery.php and edit the variables found in the top of the script.
   2. Upload ck-gallery.php to your web server.
-  3. Insert the following code to your page where you would like the gallery 
+  3. Insert the following code to your page where you would like the gallery
      to be displayed: <?php include_once('path-to-file/ck-gallery.php'); ?>
-  4. Open up your browser and navigate to your page, this will create the 
+  4. If using pagination, copy the "Pagination" portion of gallery.css into your
+     own style sheet and edit it to your liking.
+  5. Open up your browser and navigate to your page, this will create the
      directory structure for you if it doesn't already exist.
-  5. Upload your images to the images directory ("/gallery" by default).
-  6. Refresh the page in your browser and the script should generate thumbnails 
+  6. Upload your images to the images directory ("/gallery" by default).
+  7. Refresh the page in your browser and the script should generate thumbnails
      and display your pictures.
-  
-  
+
+
 Customizing your Gallery
 ----------------------------------------
-  
-  The CK-Gallery comes with style elements already set up, this allows for 
-  customization of the look and feel of your gallery.  All you have to do is 
-  add a CSS style sheet and you'll be off in no time.
 
-  This is the typical structure of the HTML output:
+  The CK-Gallery comes with class and id elements already in place, this allows
+  for customization of the look and feel of your gallery.  All you have to do is
+  edit the CSS style sheet, or create your own, and you'll be off in no time.
+
+  This is the typical structure of the XHTML output:
 
   <div id="gallery-wrapper">
     <div id="ck-gallery">
@@ -76,25 +111,42 @@ Customizing your Gallery
       <div class="gallery-box"><a href="#"><img src="#" alt="#" /></a></div>
     </div>
   </div>
-  
-    
+
+  Also, the pagination structure is as follows:
+
+  <ul id="ck-pagination">
+    <li class="inactive">&lt;</li>
+    <li class="current-page"><a href="?page=1">1</a></li>
+    <li><a href="?page=2">2</a></li>
+    <li><a href="?page=3">3</a></li>
+    <li><a href="?page=4">&gt;</a></li>
+    <li><a href="?page=2">&gt;</a></li>
+  </ul>
+
+
 Questions/Comments
 ----------------------------------------
 
-  If you have any questions or comments, please send me an email.
-    
-    Chris@Web-Geek.net
-  
-  
+  If you have any questions or comments, please contact me.
+
+    EMAIL: Chris@Web-Geek.net
+    AIM:   PHLAK2600
+    MSN:   Chris@ChronoStudios.com
+    YIM:   ChrisKankiewicz
+
+
 Shout Outs
 ----------------------------------------
 
-  Thanks go out to Dual <http://dualisanoob.com> for inadvertently inspiring me 
+  Thanks go out to Dual <http://dualisanoob.com> for inadvertently inspiring me
   to get up off my ass and program this.
-  
+
   Also, thanks to Penguin <http://www.blastwavelabs.com> for answering questions
-  throughout the entire development process!
-  
+  throughout the entire development process and for some bug testing.
+
+  Thanks also go to Nak <http://www.wetwarehacks.com> for rigorous beta testing
+  helping me iron out a number of bugs and fix backwards capabilities.
+
   Lastly, thanks to the StackOverflow.com community for help here and there.
 
 
@@ -102,7 +154,7 @@ Legal Crap
 ----------------------------------------
 
   The CK-Gallery is licensed under the GNU General Public License version 3.0.
-  
+
   Copyright (C) 2008 Chris Kankiewicz
 
   This program is free software: you can redistribute it and/or modify
@@ -117,6 +169,7 @@ Legal Crap
 
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  
-  
-  For details on Thickbox, please visit http://jquery.com/demo/thickbox
+
+  Thick box is not owned, operated, developed or maintained by Chris Kankiewicz,
+  and is in no way affiliated with CK-Gallery.  To find out more information on
+  Thickbox, please visit http://jquery.com/demo/thickbox
